cb9514a1a830e487971defd1650e1f8a0417246b
Mahananda Bose <mahananda96@gmail.com>
2020-11-18 19:47:57 +0530

    Commit

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,112 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger','stopwords'])
+
+# import libraries
+import pandas as pd
+import pickle
+import re
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import precision_score, recall_score, classification_report, accuracy_score, f1_score
 
 
 def load_data(database_filepath):
-    pass
+    
+    '''
+    Load data from database as dataframe
+    Input:
+        database_filepath: File path of sql database
+    Output:
+        X: Message data (features)
+        Y: Categories (target)
+        category_names: Labels for 36 categories
+    '''
+        
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('Disaster_Pipeline_Data',con=engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    
+    return X,Y,category_names
 
 
 def tokenize(text):
-    pass
+    
+    '''
+    Tokenize and Lemmatize text
+    Input:
+        text: original text message
+    Output:
+        tokens: Tokenized, Normalized and lemmatized text
+    '''
+    # Normalize text
+    text = re.sub(r"[^a-zA-Z0-9]", ' ', text.lower())
+    
+    # Tokenize text
+    tokens = word_tokenize(text)
+    
+    # Listing Stopwords from the text
+    stop_words = stopwords.words("english")
+    
+    # Initializing Lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    
+    # Lemmatizing words except stopwords
+    clean_tokens = [lemmatizer.lemmatize(tok).lower() for tok in tokens if tok not in stop_words]
+    
+    return tokens
 
 
 def build_model():
-    pass
+    '''
+    Build the ML Model
+    Input:
+        None
+    Output:
+        model: Return thw ML model
+    '''
+    # Declaring Pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf',MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    # Declaring parameters
+    parameters = {#'clf__estimator__n_estimators': [50, 100,200],
+        'tfidf__use_idf': (True, False),
+                  'clf__estimator__min_samples_split': [2, 3, 4]
+                 }
+
+    
+    # Declaring GRidSearch
+    cv = GridSearchCV(pipeline,parameters)
+    
+    return cv
+    
+    
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    
+    for i in range(len(category_names)):
+        print("Category:", category_names[i],"\n", classification_report(Y_test.iloc[:, i].values, Y_pred[:, i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, "wb"))
 
 
 def main():
